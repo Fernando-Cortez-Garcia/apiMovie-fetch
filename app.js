9d5714fb961508f26837c47aa2895d331d8b1cb3
Fernando Cortez <fernandolcortezlgarcia@gmail.com>
2022-06-21 14:25:47 -0500

    agregando ty and catch

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const cargarPeliculas = async() => {
-    const respuesta = await fetch('https://api.themoviedb.org/3/movie/550?api_key=a9f8786373b5ffece405294946395656');
-    console.log(respuesta);
+
+    try {
+        const respuesta = await fetch('https://api.themoviedb.org/3/movie/550?api_key=a9f8786373b5ffece40529494695656');
+        console.log(respuesta);
+        if (respuesta.status == 200) {
+            const datos = await respuesta.json();
+            console.log(datos.title);
+        } else if (respuesta.status == 401) {
+            console.log("Error: Llave invalida");
+        } else if (respuesta.status == 404) {
+            console.log('Error:Pelicula no encontrada');
+        }
+
+    } catch (err) {
+
+
+    }
 }
+
+
 cargarPeliculas();
